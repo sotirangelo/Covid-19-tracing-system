e2f144da08305b34ed3b6600304fc6fea400c207
sotirangelo <72861619+sotirangelo@users.noreply.github.com>
2020-10-23 19:25:15 +0300

    Created Test.java
    Testing github.

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+	public static String Sotiris() {
+    return "Στέφανος Χίος";
+  }
+  
+  
+  
+  public static void main(String[] args) {
+    System.out.print(Sotiris());
+  }
+  
+}
