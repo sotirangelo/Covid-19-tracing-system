0b698d158fbd689efd6b5cab6bc86f0506c33879
sotirangelo <72861619+sotirangelo@users.noreply.github.com>
2020-11-20 21:04:34 +0000

    Create Business and Person classes

---
+++ Business.java
@@ -0,0 +1,13 @@
+public class Business {
+	private double space;
+	//getBusinessType
+	//getWorkSchedule
+
+	public double getSpace() {
+		return space;
+	}
+
+	public void setSpace(double space) {
+		this.space = space;
+	}
+}
