c5354dbbe56e2f47e4a7d89752da34e1c72ab4a3
sotirangelo <t8190001@aueb.gr>
2020-11-20 14:30:07 +0000

    Add class-level Javadoc comments

--- Interface.java
+++ Interface.java
@@ -1,3 +1,19 @@
+/*
+ * Interface
+ *
+ * Copyright (not) 2020 Javavirus
+ */
+
+/**
+ * Temporary command-line Input and Output.
+ * This class consists exclusively of static methods that handle user-interface.
+ *
+ * @version 0.1 20 Nov 2020
+ * @author Sotiris
+ * @author author2
+ * @author author3
+ */
 public class Interface {
 
 }
+
