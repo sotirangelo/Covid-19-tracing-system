35d7a5eaa754566d400f96403f6a47f37b8386a5
sotirangelo <72861619+sotirangelo@users.noreply.github.com>
2020-11-21 17:51:15 +0000

    Add javadoc to Exertion.java and Mask.java

--- Exertion.java
+++ Exertion.java
@@ -1,6 +1,16 @@
+/**
+ * Exertion/Activity.
+ * This enumeration represents the types of exertion that people make.
+ * The level of exertion and consequently the breathing flow rate of each person,
+ * varies, depending on their activity. Thus, each level of exertion/activity is defined
+ * by a volumetric breathing flow rate, expressed in cubic metres per hour.
+ */
 public enum Exertion {
+	/** Resting */
 	RESTING(0.49,"Resting"),
+	/** Standing, as a low-level activity*/
 	STANDING(0.54,"Standing"),
+	/** Walking, as a medium-level activity*/
 	WALKING(1.38,"Walking");
 	
 	private final double breathingFlowRate ;
--- Mask.java
+++ Mask.java
@@ -1,7 +1,19 @@
+/**
+ * Mask types used.
+ * This enumeration represents the types of mask that can be used.
+ * Each type of mask is defined by an efficiency percentage.
+ */
 public enum Mask {
+	/** No mask used*/
 	NONE (0,"None"),
+
+	/** Fabric mask*/
 	FABRIC (0.5, "Fabric"),
+	
+	/** Medical mask*/
 	MEDICAL (0.75,"Medical"),
+	
+	/** Respirator*/
 	RESPIRATOR (0.95, "Respirator");
 
 	private final double efficiency;
@@ -12,6 +24,10 @@ public enum Mask {
 		this.type = type;
 	}
 
+	/**
+	 * Returns the efficiency of the specified mask type.
+	 * @param  double  percentage representing efficiency
+	 */
 	public double getEfficiency() {
 		return efficiency;
 	}
