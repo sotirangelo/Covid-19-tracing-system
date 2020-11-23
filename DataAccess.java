86b705a69adae94a0f99de71d062610157db87a5
sotirangelo <72861619+sotirangelo@users.noreply.github.com>
2020-11-23 16:52:13 +0000

    Create DataAccess.java

---
+++ DataAccess.java
@@ -0,0 +1,23 @@
+/*
+ * DataAccess
+ *
+ * Copyright (not) 2020 Javavirus
+ */
+
+import java.util.ArrayList;
+
+/**
+ * Temporary substitute database interaction.
+ * This class consists exclusively of static methods and fields.
+ * Its purpose is to interact with databases (currently substituted by arraylists).
+ *
+ * @version 0.1 23 Nov 2020
+ * @author Sotiris
+ * @author
+ * @author
+ * @author
+ */
+public class DataAccess {
+
+}
+
