cf70b91d76a36e0a756ed75927c52761a8d9e9e0
sotirangelo <72861619+sotirangelo@users.noreply.github.com>
2020-11-25 18:19:40 +0000

    Update BusinessType.java
    Add capacity to each business type
    TODO: Fix restaurant and bar capacity

--- BusinessType.java
+++ BusinessType.java
@@ -1,27 +1,43 @@
 /**
  * Business Types
  * This enumeration represents the different public-space/business types.
- * Each type of business is defined by
+ * Each type of business is defined by a capacity, expressed in square metres
+ * per person.
  *
  * @version 0.1 23 Nov 2020
  * @author Sotiris
  */
 public enum BusinessType {
-	RESTAURANT("Restaurant"),
-	BAR("Bar"),
-	OFFICE("Office"),
-	STORE("Store"),
-	MARKET("Market");
+	//TODO fix restaurant and bar capacity
+	RESTAURANT(-1, "Restaurant"),
+	BAR(-1, "Bar"),
+	OFFICE(10, "Office"),
+	STORE(8, "Store"),
+	MARKET("15, Market");
 
 	private final String type;
 
-	private BusinessType(String type) {
+	/** Square metres distance per person */
+	private final int capacity;
+
+	private BusinessType(int capacity, String type) {
 		this.type = type;
+		this.capacity = capacity;
 	}
 
 	@Override
 	public String toString() {
 		return type;
 	}
+
+	/** Capacity according to business type.
+	 * This method returns the capacity of a business type,
+	 * expressed in square metres per person.
+	 *
+	 * @return int Square metres
+	 */
+	public int getCapacity() {
+		return capacity;
+	}
 }
-	
+
