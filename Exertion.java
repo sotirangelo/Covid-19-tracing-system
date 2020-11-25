15ca7694057df18025c45adab0e56a7d3b482c6b
sotirangelo <72861619+sotirangelo@users.noreply.github.com>
2020-11-25 15:37:02 +0000

    Update Exertion.java & Mask.java
    - Add COVID-19 transmission metre, based on mask usage and exertion type
    
    - Update Javadoc in each file

--- Exertion.java
+++ Exertion.java
@@ -2,28 +2,32 @@
  * Exertion/Activity.
  * This enumeration represents the types of exertion that people make.
  * The level of exertion and consequently the breathing flow rate of each person,
- * varies, depending on their activity. Thus, each level of exertion/activity is defined
- * by a volumetric breathing flow rate, expressed in cubic metres per hour.
+ * varies, depending on their activity. Thus, each level of exertion, is defined
+ * by a metre ranging from 0 to 0.5 (higher means safer against COVID-19)
  *
  * @version 0.1 21 Nov 2020
  * @author Sotiris
  */
 public enum Exertion {
 	/** Resting */
-	RESTING(0.49,"Resting"),
+	RESTING(0.5,"Resting"),
 	/** Standing, as a low-level activity*/
-	STANDING(0.54,"Standing");
+	STANDING(0,"Standing");
 	
-	private final double breathingFlowRate ;
+	private final int exertionFactor ;
 	private final String type;
 
-	private Exertion(double breathingFlowRate, String type) {
-		this.breathingFlowRate = breathingFlowRate;
+	private Exertion(double exertionFactor, String type) {
+		this.exertionFactor = exertionFactor;
 		this.type = type;
 	}
 
-	public double getBreathingFlowRate() {
-		return breathingFlowRate;
+	/**
+	 * Returns the exertion factor in transmission.
+	 * @return int  COVID-19 transmission metre based on activity/exertion. 
+	 */
+	public int getExertionFactor() {
+		return exertionFactor;
 	}
 	
 	@Override
--- Mask.java
+++ Mask.java
@@ -1,7 +1,8 @@
 /**
  * Mask types used.
  * This enumeration represents the types of mask that can be used.
- * Each type of mask is defined by an efficiency percentage.
+ * Each type of mask is defined by an efficiency metre, ranging from 0 to 3, depending on the mask type.
+ * (higher means safer against COVID-19)
  *
  * @version 0.1 21 Nov 2020
  * @author Sotiris
@@ -11,25 +12,25 @@ public enum Mask {
 	NONE (0,"None"),
 
 	/** Fabric mask*/
-	FABRIC (0.5, "Fabric"),
+	FABRIC (1, "Fabric"),
 	
 	/** Medical mask*/
-	MEDICAL (0.75,"Medical"),
+	MEDICAL (2,"Medical"),
 	
 	/** Respirator*/
-	RESPIRATOR (0.95, "Respirator");
+	RESPIRATOR (3, "Respirator");
 
-	private final double efficiency;
+	private final int efficiency;
 	private final String type;
 
-	private Mask(double efficiency, String type) {
+	private Mask(int efficiency, String type) {
 		this.efficiency = efficiency;
 		this.type = type;
 	}
 
 	/**
 	 * Returns the efficiency of the specified mask type.
-	 * @param  double  percentage representing efficiency
+	 * @param  int  COVID-19 transmission metre based on mask usage.
 	 */
 	public double getEfficiency() {
 		return efficiency;
