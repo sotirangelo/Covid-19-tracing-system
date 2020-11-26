0855712a755842c3e5a6a497edbf8a57b27bba6b
seraoik <72869715+seraoik@users.noreply.github.com>
2020-11-26 22:50:57 +0200

    Create Age.java
    A first try in the enumeration of the customers age group. (age group may vary, after consulting our scientific database

---
+++ Age.java
@@ -0,0 +1,44 @@
+/**
+ * Customer's age group.
+ * This enumeration represents the customer's age group that can be used.
+ * Each age group is defined by an efficiency metre, ranging from 0 to 3, depending on the mask type.
+ * (higher means more vulnerable against COVID-19)
+ *
+ * @version 0.1 26 Nov 2020
+ * @author Serafeim
+ */
+ 
+ 
+public enum Age {
+	/** Age 1-18*/
+	UNDERAGE (0,"Underage"),
+
+	/** Age 19-30*/
+	EIGHTEEN (1, "Above 18"),
+	
+	/** Age 31-50*/
+	THIRTY (2,"Above Thirty"),
+	
+	/** Age 51-100*/
+	FIFTY (3, "Above Fifty");
+
+	private final int vulnerability;
+	private final int age;
+
+	private (int vulnerability, int age) {
+		this.vulnerability = vulnerability;
+		this.age = age;
+	}
+
+	/**
+	 * Returns the vulnerability of the customer to COVID 19.
+	 * @param  int  COVID-19 transmission metre based on mask usage.
+	 */
+	public double getVulnerability() {
+		return vulnerability;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+}
