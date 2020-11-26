066b75c682964ec410282632567278375b092eaa
John Sandalis <72869500+JohnSandalis@users.noreply.github.com>
2020-11-26 03:24:32 +0200

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,14 +1,35 @@
+/*
+ * Person
+ *
+ * Copyright (not) 2020 Javavirus
+ */
+import java.time.LocalDateTime; // Import the LocalDateTime class
+import java.time.format.DateTimeFormatter; // Import the DateTimeFormatter class
+
+/**
+ * This class consists exclusively of static methods and fields.
+ * Its purpose is to get or set values for a "person" object.
+ *
+ * @version 0.1 26 Nov 2020
+ * @author Sotiris
+ * @author Ioannis
+ * 
+ */
+
 public class Person {
+	/** Temporary field that formats Date objects in String "dd-MM-yyyy HH:mm:ss" format . Has not been used yet*/
+	static DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+	
 	private String firstName;
 	private String lastName;
 	private int age;
 	private String email;
 	private int phoneNumber;
-	//getMaskType
-	//getEntryTime
-	//getExitTime
-	//getActivity
-	
+	private Mask maskType;
+	private Exertion exertionType;
+	private LocalDateTime entryTime;
+	private LocalDateTime exitTime;
+
 	public String getFirstName() {
 		return firstName;
 	}
@@ -39,6 +60,31 @@ public class Person {
 	public void setPhoneNumber(int phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
+	public Mask getMask() {
+        return maskType;
+	}
+	public void setMask(Mask mask) {
+        this.maskType = mask;
+	}
+	public Exertion getExertion() {
+		return exertionType;
+	}
+	public void setExertion(Exertion exertion) {
+		this.exertionType = exertion;
+	}
+	public LocalDateTime getEntryTime() {
+		return entryTime;
+	}
+	public void setEntryTime(LocalDateTime time) {
+		this.entryTime = time;
+	}
+	public LocalDateTime getExitTime() {
+		return exitTime;
+	}
+	public void setExitTime(LocalDateTime time) {
+		this.exitTime = time;
+	}
+
 
 }
-	
+
