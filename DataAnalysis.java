5ea2bc2319bf77cf71462db4cd29ea1059e13562
sotirangelo <72861619+sotirangelo@users.noreply.github.com>
2020-11-28 13:56:03 +0000

    Create DataAnalysis.java

---
+++ DataAnalysis.java
@@ -0,0 +1,23 @@
+/*
+ * DataAnalysis
+ *
+ * Copyright (not) 2020 Javavirus
+ */
+
+import java.util.ArrayList;
+
+/**
+ * Analysis of data (temporarily from arraylists).
+ * This class consists exclusively of static methods and fields.
+ * Its purpose is to analyze data taken from th embedded database (currently substituted by arraylists).
+ *
+ * @version 0.1 23 Nov 2020
+ * @author Sotiris
+ * @author Nefeli
+ * @author
+ * @author
+ */
+public class DataAnalysis {
+
+}
+
