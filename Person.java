611eb9e00931bd397b385e0eb5ae8af0d25af4a6
John Sandalis <72869500+JohnSandalis@users.noreply.github.com>
2020-11-30 18:06:30 +0200

    Removed Age,Entry/ExitTime,ExertionType,MaskType

--- Person.java
+++ Person.java
@@ -4,11 +4,10 @@
  * Copyright (not) 2020 Javavirus
  */
 
-import java.util.Date;
-
 /**
- * Person class.
- * This class represents a person.
+ * Person class
+ * This class consists exclusively of static methods and fields.
+ * Its purpose is to get or set values for a "person" object.
  *
  * @version 0.1 26 Nov 2020
  * @author Sotiris
@@ -20,14 +19,17 @@ public class Person {
 	
 	private String firstName;
 	private String lastName;
-	private int age;
 	private String email;
 	private int phoneNumber;
-	private Mask maskType;
-	private Exertion exertionType;
-	private Date entryTime;
-	private Date exitTime;
 	private Age ageCategory;
+	
+	public Person(String firstName, String lastName, String email, int phoneNumber, Age ageCategory) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.ageCategory = ageCategory;
+	}
 
 	public String getFirstName() {
 		return firstName;
@@ -41,12 +43,6 @@ public class Person {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
 	public String getEmail() {
 		return email;
 	}
@@ -58,30 +54,6 @@ public class Person {
 	}
 	public void setPhoneNumber(int phoneNumber) {
 		this.phoneNumber = phoneNumber;
-	}
-	public Mask getMask() {
-        return maskType;
-	}
-	public void setMask(Mask mask) {
-        this.maskType = mask;
-	}
-	public Exertion getExertion() {
-		return exertionType;
-	}
-	public void setExertion(Exertion exertion) {
-		this.exertionType = exertion;
-	}
-	public Date getEntryTime() {
-		return entryTime;
-	}
-	public void setEntryTime(Date time) {
-		this.entryTime = time;
-	}
-	public Date getExitTime() {
-		return exitTime;
-	}
-	public void setExitTime(Date time) {
-		this.exitTime = time;
 	}
 	public Age getAgeCategory() {
 		return ageCategory;
