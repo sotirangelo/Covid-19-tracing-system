ac9b08a3dc170d25519089d96a670923080c9565
kmaragkos <72869765+kmaragkos@users.noreply.github.com>
2020-12-02 00:08:23 +0200

    Create InfectedPerson.java

---
+++ InfectedPerson.java
@@ -0,0 +1,12 @@
+
+
+Public class InfectedPerson etxends Person { 
+  private double score; 
+    public static double getScore () { 
+      return this.score; 
+      }
+    public static void setScore (double x) { 
+      this.score = x; 
+      }
+ }
+        
