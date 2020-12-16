a8ad961fddefa28a1b1bb8c8d7cc8b1f27ee3fa5
kmaragkos <72869765+kmaragkos@users.noreply.github.com>
2020-12-17 00:38:02 +0200

    Update MyButton.java

--- MyButton.java
+++ MyButton.java
@@ -1,7 +1,7 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.ActionListener; // The ActionListener that will be used to "listen the button" 
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
