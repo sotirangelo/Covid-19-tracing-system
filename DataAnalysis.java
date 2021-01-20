117d9d83e457ae58b9162fa6bddea65678675c73
sotirangelo <72861619+sotirangelo@users.noreply.github.com>
2021-01-20 03:22:42 +0200

    Update DB_Access & DataAnalysis
    DB_Access:
    
    - Add getBusinessDayRecords()
    
    - Add isUserIDInfected(), to validate whether Person with specified
    UserID is infected or not.
    
    
    DataAnalysis:
    
    - Update calculateErq(), so that it may be called multiple times (to
    cover the possible outcome, that multiple infected people visit the
    store)
    
    - Add calculateTotalErq(), that returns the erq table of the given
    business, for the entire day of the infected person's visit
    
    - Update calculatePI(), to reflect the new changes in the previously
    mentioned methods. Needs fixing!
    
    - Add getBusinesDayDuration()
    
    - Add getStartRecord() and getLastRecord()

--- DataAnalysis.java
+++ DataAnalysis.java
@@ -4,6 +4,7 @@
  * Copyright (not) 2020 Javavirus
  */
 
+import java.sql.Timestamp;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -86,43 +87,58 @@ public class DataAnalysis {
 		}
 	}
 */
-	
-	public static ArrayList<InfectedPerson> calculateErq (InfectedPerson infected, Business business) {
-		
-		Record record1 = DB_Access.getPersonsRecord(infected, business); //ONLY NEED FIRST RECORD (IN CASE OF MULTIPLE)
-		
-		ArrayList<Record> dayrecords = getBusinessDayRecords(LocalDateTime record1.getEntryDate()); //LocalDateTime or Timestamp
-		//DIFFERENT METHODS
-		//get start and end dates
-		Record start;
-		Record end;
-		for (int i = 0; i < dayrecords.size(); i++) {
-			if (i == 0) {
-				start = dayrecords.get(i);
-				end = dayrecords.get(i);
-			} else {
-				if (start.getEntryDate().after(dayrecords.get(i).getEntryDate())) {
-					start = dayrecords.get(i);
-				}
-				if (end.getExitDate().before(dayrecords.get(i).getExitDate())) {
-					end = dayrecords.get(i);
+	public static ArrayList<InfectedPerson> calculatePI(InfectedPerson infected, Business business) { //TODO: Fix necessary stuff
+		var records = DB_Access.getRecords(business.getBusinessID());
+		ArrayList<InfectedPerson> output = new ArrayList<InfectedPerson>();
+		double[] erq = calculateTotalErq(infected, business);
+		for (Record r : records) { //TODO: Get only useful records
+			double p = 0;
+			for (int i = r.getEntry() ; i < erq.length ; i++) {
+				if (i <= r.getExit()) {
+					p += erq[i] * ((i / 60) - ((i - 1) / 60));		
 				}
 			}
+			p *= business.getIR();
+			if (p > 0) {
+				output.add((InfectedPerson) r.getPerson()); //TODO: Fix Infected Person casing
+			}
 		}
-		//Import LocalDateTime
+	}
 	
-		Record record; //Infected record parameter
+	public static double[] calculateTotalErq(InfectedPerson infected, Business business) {
+		Record covidrecord = DB_Access.getPersonsRecord(infected, business); //ONLY NEED FIRST RECORD (IN CASE OF MULTIPLE)
+		double[] totalerq = new double[(int) getBusinessDayDuration(covidrecord).toMinutes()];
+		for (int i = 0; i < 0; i++) { //XXX: Fill totalerq with zeros
+			totalerq[i] = 0;
+		}
+		ArrayList<Record> dayrecords = DB_Access.getBusinessDayRecords((Timestamp) covidrecord.getEntryDate());
+		for (Record r : dayrecords) {
+			try {
+				if (DB_Access.isUserIDInfected(r.getUserID())) {
+					double[] erq = calculateErq(r);
+					for (int i = 0; i < erq.length; i++) {
+						totalerq[i] += erq[i];
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return totalerq;
+		
+	}
+	
+	private static double[] calculateErq (Record record) {
+		LocalDateTime startTime = convertToLocalDateTime(getStartRecord(record.getEntryDate()).getEntryDate());
 		Business business = DB_Access.findBusiness(record.getBusinessID());
 		double activity = getActivity(DB_Access.findUser(record.getUserID()), business);
-		LocalDateTime startTime= convertToLocalDateTime(start.getEntryDate());//TODO: convertToLocalDateTime(Date dateToConvert)
-		LocalDateTime endTime = convertToLocalDateTime(end.getExitDate());
 		LocalDateTime recordEntryTime = convertToLocalDateTime(record.getEntryDate());
 		LocalDateTime recordExitTime = convertToLocalDateTime(record.getExitDate());
-		Duration businessDuration = Duration.between(startTime, endTime);
-		double[] erq = new double[(int) businessDuration.toMinutes()];	
+		Duration businessDayDuration = getBusinessDayDuration(record);
+		double[] erq = new double[(int) businessDayDuration.toMinutes()];	
 		int entry = (int) Duration.between(startTime, recordEntryTime).toMinutes();
 		int exit = (int) Duration.between(startTime, recordExitTime).toMinutes();
-		for (int counter = 0; counter <= (int) businessDuration.toMinutes(); counter++) {
+		for (int counter = 0; counter <= (int) businessDayDuration.toMinutes(); counter++) {
 			if (counter < entry) {
 				erq[counter] = 0;
 			} else if (counter < exit) {
@@ -141,10 +157,45 @@ public class DataAnalysis {
 	      .toLocalDateTime();
 	}
 	
-	private static getMinMaxRecords() {
-		
+	private static Duration getBusinessDayDuration(Record record) {
+		Record start = getStartRecord(record.getEntryDate()); //start of business hours
+		Record end = getLastRecord(record.getEntryDate()); //end of business hours		
+		Business business = DB_Access.findBusiness(start.getBusinessID());
+		LocalDateTime startTime= convertToLocalDateTime(start.getEntryDate());//TODO: convertToLocalDateTime(Date dateToConvert)
+		LocalDateTime endTime = convertToLocalDateTime(end.getExitDate());
+		Duration businessDayDuration = Duration.between(startTime, endTime);
+		return businessDayDuration;
 	}
 	
+	private static Record getStartRecord(Date date) {
+		ArrayList<Record> dayrecords = DB_Access.getBusinessDayRecords((Timestamp) date);//record1.getEntryDate
+		Record start = null;
+		for (int i = 0; i < dayrecords.size(); i++) {
+			if (i == 0) {
+				start = dayrecords.get(i);
+			} else {
+				if (start.getEntryDate().after(dayrecords.get(i).getEntryDate())) {
+					start = dayrecords.get(i);
+				}
+			}
+		}
+		return start;
+	}
+	
+	private static Record getLastRecord(Date date) {
+		ArrayList<Record> dayrecords = DB_Access.getBusinessDayRecords((Timestamp) date);//record1.getEntryDate
+		Record end = null;
+		for (int i = 0; i < dayrecords.size(); i++) {
+			if (i == 0) {
+				end = dayrecords.get(i);
+			} else {
+				if (end.getExitDate().before(dayrecords.get(i).getExitDate())) {
+					end = dayrecords.get(i);
+				}
+			}
+		}
+		return end;
+	}
 /*
 	public static ArrayList<InfectedPerson> infectionScores(String userID) {
 		ArrayList<InfectedPerson> ip = new ArrayList<InfectedPerson>();
