5ff6ed84462ecb93fc9ca37bfa74c53c1001054f
seraoik <72869715+seraoik@users.noreply.github.com>
2021-01-24 12:51:52 +0200

    Update LauncherController.java

--- LauncherController.java
+++ LauncherController.java
@@ -5,6 +5,7 @@ package application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -16,7 +17,7 @@ public class LauncherController {
 	/* Labels */
 	
 	@FXML
-	private Label lbl1AppName1;
+	private Label lbl1AppName;
 	
 	@FXML
 	private Label lbl2Welcome;
@@ -39,25 +40,25 @@ public class LauncherController {
 	@FXML
 	private Button btn4Exit;
 
-	public void Launcher(ActionEvent event) throws Exception {
-		Stage primaryStage = new Stage();
-		Parent root = FXMLLoader.load(getClass().getResource("/application/Customer.fxml"));
-		Scene scene = new Scene(root,417,79);
-		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		
-	}
-	
-	
 	
 	public void exitButtonOnAction(ActionEvent event) {
 		Stage primaryStage = (Stage) btn4Exit.getScene().getWindow();
 		primaryStage.close();
 	}
 	
+	public void OutputButtonOnAction(ActionEvent event) {
+		((Node)event.getSource()).getScene().getWindow().hide();
+		createOutput();
+	}
+	
 	public void CustomerButtonOnAction(ActionEvent event) {
+		((Node)event.getSource()).getScene().getWindow().hide();
 		createCustomer();
+	}
+	
+	public void BusinessButtonOnAction(ActionEvent event) {
+		((Node)event.getSource()).getScene().getWindow().hide();
+		createBusiness();
 	}
 	
 	public void createCustomer() {
@@ -66,12 +67,42 @@ public class LauncherController {
 		Parent root = FXMLLoader.load(getClass().getResource("/application/Customer.fxml"));
 		Scene scene = new Scene(root,400,400);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		customerStage.setTitle("Customer Log In");
+		customerStage.setFullScreen(true);
 		customerStage.setScene(scene);
 		customerStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
+		}
 		
 	}
+	public void createBusiness() {
+		try {
+		Stage businessStage = new Stage();
+		Parent root = FXMLLoader.load(getClass().getResource("/application/Business.fxml"));
+		Scene scene = new Scene(root,400,400);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		businessStage.setTitle("Business Log In");
+		businessStage.setFullScreen(true);
+		businessStage.setScene(scene);
+		businessStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
+	public void createOutput() {
+		try {
+		Stage outputStage = new Stage();
+		Parent root = FXMLLoader.load(getClass().getResource("/application/Output.fxml"));
+		Scene scene = new Scene(root,400,400);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		outputStage.setTitle("Output");
+		outputStage.setFullScreen(true);
+		outputStage.setScene(scene);
+		outputStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
