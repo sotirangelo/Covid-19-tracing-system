b1f7387b9dbfc94971108d8e5cd7a1c79c65ddc6
seraoik <72869715+seraoik@users.noreply.github.com>
2021-01-24 12:52:30 +0200

    Update CustomerController.java

--- CustomerController.java
+++ CustomerController.java
@@ -1,13 +1,18 @@
 package application;
 
-	import javafx.event.ActionEvent;
+	import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javafx.event.ActionEvent;
 	import javafx.fxml.FXML;
 	import javafx.fxml.FXMLLoader;
-	import javafx.scene.Parent;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 	import javafx.scene.Scene;
 	import javafx.scene.control.Button;
 	import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 	public class CustomerController {
@@ -18,13 +23,18 @@ import javafx.stage.Stage;
 		private Label lbl1Title;
 		
 		@FXML
-		private Label lbl2EnterID;
-		
-		@FXML
 		private Label lbl3UserID;
 		
 		@FXML
 		private Label lbl4NewHere;
+		
+		@FXML
+		private Label lbl5CustomerLogIn;
+		
+		@FXML
+		private Label lbl6UserLogInStatus;
+		
+		
 	
 	
 		
@@ -47,22 +57,14 @@ import javafx.stage.Stage;
 		
 		
 
-
-		public void Customer(ActionEvent event) throws Exception {
-			Stage customerStage = new Stage();
-			Parent root = FXMLLoader.load(getClass().getResource("/application/CustomerSignUp.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			customerStage.setScene(scene);
-			customerStage.show();
-		}
-		
 		public void exitButtonOnAction(ActionEvent event) {
 			Stage customerStage = (Stage) btn3Exit.getScene().getWindow();
+			((Node)event.getSource()).getScene().getWindow().hide();
 			customerStage.close();
 		}
 		
 		public void CustomerSignUpButtonOnAction(ActionEvent event) {
+			((Node)event.getSource()).getScene().getWindow().hide();
 			createCustomerSignUp();
 		}
 		
@@ -72,15 +74,13 @@ import javafx.stage.Stage;
 			Parent root = FXMLLoader.load(getClass().getResource("/application/CustomerSignUp.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			customerSignUpStage.setFullScreen(true);
+			customerSignUpStage.setTitle("Customer Sign Up");
 			customerSignUpStage.setScene(scene);
 			customerSignUpStage.show();
 			} catch(Exception e) {
 				e.printStackTrace();
-			
-		}
-		
-		}
-	
-
+			}
+		}	
 	}
-
+		
