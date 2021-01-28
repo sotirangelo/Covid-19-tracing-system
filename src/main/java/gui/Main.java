f0d18920f6607066df45f8adcbb8506fc80c2515
sotirangelo <72861619+sotirangelo@users.noreply.github.com>
2021-01-29 00:55:57 +0200

    Replace BufferedImage with Image in Main

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -1,10 +1,5 @@
 package gui;
-	
-import java.awt.image.BufferedImage;
-import java.io.InputStream;
 
-import javax.imageio.ImageIO;
-import javafx.embed.swing.SwingFXUtils;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -23,8 +18,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root,552,339);
 			scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
 			businessStage.setScene(scene);
-			BufferedImage icon = ImageIO.read(getClass().getResourceAsStream("/images/Javavirus Logo.png"));
-			businessStage.getIcons().add(SwingFXUtils.toFXImage(icon, null));
+			businessStage.getIcons().add(new Image("/images/Javavirus Logo.png"));
 			businessStage.setTitle("Javavirus Covid19 Tracing App - Business Log In");
 			businessStage.show();
 	}
